4f200b749f947e02d74c7483ea465ab82a8d3e29
NukeZzZin _ <nukezzzin@gmail.com>
2022-05-04 15:23:36 -0300

    ✨ Update 'logger.ts' ✨

--- src/modules/logger.ts
+++ src/modules/logger.ts
@@ -13,25 +13,25 @@ enum WriteTypes {
 
 // TODO: performance improvement.
 class Logger {
-    public static success(message?: any, ..._message: any[]): void {
-		return console.log(`[${chalk.green("SUCCESS")}] ${chalk.bold.black(moment().format("HH:MM:SS"))} ${util.format(message)} ${util.format(..._message)}`);
+    public static success(message: any, ..._message: any[]): boolean {
+		return process.stdout.write(`[${chalk.green("SUCCESS")}] ${chalk.bold.black(moment().format("HH:MM:SS"))} ${util.format(message)} ${util.format(..._message)}\r\n`);
     }
 
-    public static error(message?: any, ..._message: any[]): void {
-		return console.log(`[${chalk.red("ERROR")}] ${chalk.bold.black(moment().format("HH:MM:SS"))} ${util.format(message)} ${util.format(..._message)}`);
+    public static error(message: any, ..._message: any[]): boolean {
+		return process.stdout.write(`[${chalk.red("ERROR")}] ${chalk.bold.black(moment().format("HH:MM:SS"))} ${util.format(message)} ${util.format(..._message)}\r\n`);
 	}
 
-	public static info(message?: any, ..._message: any[]): void {
-		return console.log(`[${chalk.cyan("INFO")}] ${chalk.bold.black(moment().format("HH:MM:SS"))} ${util.format(message)} ${util.format(..._message)}`);
+	public static info(message: any, ..._message: any[]): boolean {
+		return process.stdout.write(`[${chalk.cyan("INFO")}] ${chalk.bold.black(moment().format("HH:MM:SS"))} ${util.format(message)} ${util.format(..._message)}\r\n`);
 	}
 
-	public static test(message?: any, ..._message: any[]): void {
-		return console.log(`[${chalk.yellow("TEST")}] ${chalk.bold.black(moment().format("HH:MM:SS"))} ${util.format(message)} ${util.format(..._message)}`);
+	public static test(message: any, ..._message: any[]): boolean {
+		return process.stdout.write(`[${chalk.yellow("TEST")}] ${chalk.bold.black(moment().format("HH:MM:SS"))} ${util.format(message)} ${util.format(..._message)}\r\n`);
 	}
 
-    public static async write(mode: WriteTypes = WriteTypes.Error, message?: any, ..._message: any[]): Promise<boolean> {
+    public static async write(mode: WriteTypes = WriteTypes.Error, message: any, ..._message: any[]): Promise<boolean> {
         const stream = await fs.createWriteStream(path.join(__dirname, `../../logs/${mode}_${moment().format("YYYY-MM-DD")}.log`), { encoding: "utf-8", flags: "a" });
-        return stream.write(`[${mode.toUpperCase()}] ${moment().toISOString(true)}: ${message}\r\n`);
+        return stream.write(`[${mode.toUpperCase()}] ${moment().toISOString(true)}: ${util.format(message)} ${util.format(..._message)}\r\n`);
     }
 }
 
